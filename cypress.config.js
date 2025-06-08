588d9247b2238c9e7796ce4349b95d6054083744
kimjayluta <kimjayluta0721@gmail.com>
2025-06-08 22:18:38 +0800

    added first login TC

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config();
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://www.saucedemo.com/',
+    env: {
+      USER_NAME: process.env.USER_NAME,
+      USER_PASSWORD: process.env.USER_PASSWORD,
+    }
   },
+  
 });
